a1df229724055dfdd175350afd5c208ce6fd84c5
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2015-02-02 07:43:33 -0200

    Settings: HeadsUp options [2/2]
    Includes:
    
    * Settings: forward port HeadsUp options [2/2]
    
    -Global on/off
    -Blacklist and Do not disturb
    
    (by Adnan Begovic)
    
    * HeadsUp settings: fix enabled state on clean install
    
    (by Altaf Patel)
    
    * My changes
    
    - Swype action selector (delete/hide)
    
    Change-Id: Ia2ecc52034f04b11590abd62c5119f736985450a
    
    Modified to temasek
    
    Conflicts:
    	AndroidManifest.xml
    
    Conflicts:
    	res/values/cr_strings.xml
    	res/xml/temasek_settings.xml
    	src/com/android/settings/temasek/TemasekSettings.java

--- src/com/android/settings/notification/NotificationManagerSettings.java
+++ src/com/android/settings/notification/NotificationManagerSettings.java
@@ -18,6 +18,7 @@ package com.android.settings.notification;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -48,6 +49,17 @@ public class NotificationManagerSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.notification_manager_settings);
         mSecure = new LockPatternUtils(getActivity()).isSecure();
         initLockscreenNotifications();
+        
+        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        boolean headsUpEnabled = Settings.System.getInt(
+                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION,1) != 0;
+        mHeadsUp.setSummary(headsUpEnabled
+               ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     // === Lockscreen (public / private) notifications ===
