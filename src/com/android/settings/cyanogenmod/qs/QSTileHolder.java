1872d4a1c6a7cd9e6cea4330b62d855e057fd7ae
STELIX <ssspinni@gmail.com>
2015-02-02 15:45:28 +0100

    Settings - Add PowerMenu QS Tile (Reboot on longpress) [2/2]
    PS2: delete unneeded drawables
         string edit
    
    Change-Id: I5c60a23f85df9d6f9e264ffba970ea8a49af5cbd
    Signed-off-by: STELIX <ssspinni@gmail.com>
    
    Conflicts:
    	res/values/cm_strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
    
    Conflicts:
    	res/values/cm_strings.xml

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -117,6 +117,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_system_profiles";
                 stringId = R.string.qs_tile_profiles;
                 break;
+            case QSConstants.TILE_POWERMENU:
+                resourceName = "ic_qs_powermenu";
+                stringId = R.string.qs_tile_powermenu;
+                break;
             case QSConstants.TILE_NFC:
                 resourceName = "ic_qs_nfc_on";
                 stringId = R.string.qs_tile_nfc;
