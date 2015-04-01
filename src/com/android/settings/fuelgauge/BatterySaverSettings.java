951c6a4156c2b37ef2261bb44acfaa778e5d4fbc
rogersb11 <brettrogers11@gmail.com>
2015-04-01 07:01:43 -0400

    Remove icon for now, not seeing it for some damn reason

--- src/com/android/settings/fuelgauge/BatterySaverSettings.java
+++ src/com/android/settings/fuelgauge/BatterySaverSettings.java
@@ -149,7 +149,6 @@ public class BatterySaverSettings extends SettingsPreferenceFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_settings_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
