VanirAOSP/packages_apps_Settings

Squash of Dave's commits for Navi (2/2)
Inception to 1/4/15.  For testing

Vanir actions and utilities [2/2]

activities for vanir actions and utilities

Change-Id: Ie8c8faed8b1309442b71899b32f5acb31ccc228d
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	AndroidManifest.xml

Customizable navbar [2/2]

Change-Id: I3481687dbd7bc7de903ab9266a0473b33185f5ff
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml

Cleanup

Tabs,whitespaces

Change-Id: I90488a92c80b764a91acc6eada8d91a2c223c9b8
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml
	res/xml/vanir_interface.xml

ArrangeNavbarFragment: fix race condition

Change-Id: Ic854476db3478d31637ac1b1fb6860ef12c88b80

Navbar icons: since DocumentsUI is a megafail lets guide users to gallery

Change-Id: Ic9b179a24634ec7ca1d407a7f9ae2a581dd9d307
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_strings.xml

Navring toggle: make sure to use the correct default value

Change-Id: I641731882792bbf3eab7cc9c9e48556b470aaa39

src/com/android/settings/vanir/activities/RingSilentToggle.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingSilentToggle extends Activity  {
+
+    public RingSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+        finish();
+    }
+}

src/com/android/settings/vanir/activities/RingVibToggle.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibToggle extends Activity  {
+
+    public RingVibToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() != AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+        finish();
+    }
+}

src/com/android/settings/vanir/activities/StartDreamActivity.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.service.dreams.DreamService;
+import android.service.dreams.IDreamManager;
+import android.widget.Toast;
+
+import com.android.settings.R;
+
+/*
+ * Start Dreams
+ */
+
+public class StartDreamActivity extends Activity {
+
+    private IDreamManager mDreamManager;
+
+    public StartDreamActivity() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDreamManager = IDreamManager.Stub.asInterface(
+                ServiceManager.getService(DreamService.DREAM_SERVICE));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mDreamManager == null)
+            return;
+        try {
+            mDreamManager.dream();
+        } catch (RemoteException e) {
+            final String cheese = StartDreamActivity.this.getString(R.string.toggle_dream_failed);
+            Toast.makeText(StartDreamActivity.this, cheese, Toast.LENGTH_SHORT).show();
+        }
+        this.finish();
+    }
+}

src/com/android/settings/vanir/util/DragGripView.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+
+import com.android.settings.R;
+
+public class DragGripView extends View {
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.gravity,
+            android.R.attr.color,
+    };
+
+    private static final int HORIZ_RIDGES = 3;
+
+    private int mGravity = Gravity.START;
+    private int mColor = 0x33333333;
+
+    private Paint mRidgePaint;
+
+    private float mRidgeSize;
+    private float mRidgeGap;
+
+    private int mWidth;
+    private int mHeight;
+
+    public DragGripView(Context context) {
+        this(context, null, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+        mGravity = a.getInteger(0, mGravity);
+        mColor = a.getColor(1, mColor);
+        a.recycle();
+
+        final Resources res = getResources();
+        mRidgeSize = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_size);
+        mRidgeGap = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_gap);
+
+        mRidgePaint = new Paint();
+        mRidgePaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(
+                View.resolveSize(
+                        (int) (HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap)
+                                + getPaddingLeft() + getPaddingRight(),
+                        widthMeasureSpec),
+                View.resolveSize(
+                        (int) mRidgeSize,
+                        heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float drawWidth = HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawLeft;
+
+        switch (Gravity.getAbsoluteGravity(mGravity, getLayoutDirection())
+                & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                drawLeft = getPaddingLeft()
+                        + ((mWidth - getPaddingLeft() - getPaddingRight()) - drawWidth) / 2;
+                break;
+            case Gravity.RIGHT:
+                drawLeft = getWidth() - getPaddingRight() - drawWidth;
+                break;
+            default:
+                drawLeft = getPaddingLeft();
+        }
+
+        int vertRidges = (int) ((mHeight - getPaddingTop() - getPaddingBottom() + mRidgeGap)
+                / (mRidgeSize + mRidgeGap));
+        float drawHeight = vertRidges * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawTop = getPaddingTop()
+                + ((mHeight - getPaddingTop() - getPaddingBottom()) - drawHeight) / 2;
+
+        for (int y = 0; y < vertRidges; y++) {
+            for (int x = 0; x < HORIZ_RIDGES; x++) {
+                canvas.drawRect(
+                        drawLeft + x * (mRidgeSize + mRidgeGap),
+                        drawTop + y * (mRidgeSize + mRidgeGap),
+                        drawLeft + x * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        drawTop + y * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        mRidgePaint);
+            }
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHeight = h;
+        mWidth = w;
+    }
+}

CyanideL/android_packages_apps_Settings

	res/xml/dashboard_categories.xml
	res/layout/ga_create_gesture.xml
	res/layout/ga_gestures_list.xml
	src/com/android/settings/cyanide/gestureanywhere/GestureAnywhereCreateGestureActivity.java

src/com/android/settings/cyanide/activities/RingSilentToggle.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanide.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingSilentToggle extends Activity  {
+
+    public RingSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+        finish();
+    }
+}

src/com/android/settings/cyanide/activities/RingVibToggle.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanide.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibToggle extends Activity  {
+
+    public RingVibToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() != AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+        finish();
+    }
+}

src/com/android/settings/cyanide/activities/StartDreamActivity.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright 2014 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanide.activities;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.service.dreams.DreamService;
+import android.service.dreams.IDreamManager;
+import android.widget.Toast;
+
+import com.android.settings.R;
+
+/*
+ * Start Dreams
+ */
+
+public class StartDreamActivity extends Activity {
+
+    private IDreamManager mDreamManager;
+
+    public StartDreamActivity() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDreamManager = IDreamManager.Stub.asInterface(
+                ServiceManager.getService(DreamService.DREAM_SERVICE));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mDreamManager == null)
+            return;
+        try {
+            mDreamManager.dream();
+        } catch (RemoteException e) {
+            final String cheese = StartDreamActivity.this.getString(R.string.toggle_dream_failed);
+            Toast.makeText(StartDreamActivity.this, cheese, Toast.LENGTH_SHORT).show();
+        }
+        this.finish();
+    }
+}

src/com/android/settings/cyanide/util/DragGripView.java
@@ -1,0 +1,129 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanide.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+
+import com.android.settings.R;
+
+public class DragGripView extends View {
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.gravity,
+            android.R.attr.color,
+    };
+
+    private static final int HORIZ_RIDGES = 3;
+
+    private int mGravity = Gravity.START;
+    private int mColor = 0x33333333;
+
+    private Paint mRidgePaint;
+
+    private float mRidgeSize;
+    private float mRidgeGap;
+
+    private int mWidth;
+    private int mHeight;
+
+    public DragGripView(Context context) {
+        this(context, null, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragGripView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        final TypedArray a = context.obtainStyledAttributes(attrs, ATTRS);
+        mGravity = a.getInteger(0, mGravity);
+        mColor = a.getColor(1, mColor);
+        a.recycle();
+
+        final Resources res = getResources();
+        mRidgeSize = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_size);
+        mRidgeGap = res.getDimensionPixelSize(R.dimen.drag_grip_ridge_gap);
+
+        mRidgePaint = new Paint();
+        mRidgePaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(
+                View.resolveSize(
+                        (int) (HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap)
+                                + getPaddingLeft() + getPaddingRight(),
+                        widthMeasureSpec),
+                View.resolveSize(
+                        (int) mRidgeSize,
+                        heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float drawWidth = HORIZ_RIDGES * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawLeft;
+
+        switch (Gravity.getAbsoluteGravity(mGravity, getLayoutDirection())
+                & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                drawLeft = getPaddingLeft()
+                        + ((mWidth - getPaddingLeft() - getPaddingRight()) - drawWidth) / 2;
+                break;
+            case Gravity.RIGHT:
+                drawLeft = getWidth() - getPaddingRight() - drawWidth;
+                break;
+            default:
+                drawLeft = getPaddingLeft();
+        }
+
+        int vertRidges = (int) ((mHeight - getPaddingTop() - getPaddingBottom() + mRidgeGap)
+                / (mRidgeSize + mRidgeGap));
+        float drawHeight = vertRidges * (mRidgeSize + mRidgeGap) - mRidgeGap;
+        float drawTop = getPaddingTop()
+                + ((mHeight - getPaddingTop() - getPaddingBottom()) - drawHeight) / 2;
+
+        for (int y = 0; y < vertRidges; y++) {
+            for (int x = 0; x < HORIZ_RIDGES; x++) {
+                canvas.drawRect(
+                        drawLeft + x * (mRidgeSize + mRidgeGap),
+                        drawTop + y * (mRidgeSize + mRidgeGap),
+                        drawLeft + x * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        drawTop + y * (mRidgeSize + mRidgeGap) + mRidgeSize,
+                        mRidgePaint);
+            }
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHeight = h;
+        mWidth = w;
+    }
+}