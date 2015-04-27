32ddeb95b233ca178dd297e4625c6097d71b62d3
cretin45 <cretin45@gmail.com>
2015-04-27 13:55:06 -0700

    Settings: Add config for not allowing calls/sms being set to > slot 1
    This is a requirement of a vendor. The default is false.
    
    Change-Id: If3e0755c4ba3d33e3ac2d331182bc740688b8a23

--- src/com/android/settings/sim/SimSettings.java
+++ src/com/android/settings/sim/SimSettings.java
@@ -90,6 +90,7 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
     private PhoneStateListener[] mPhoneStateListener;
     private boolean mDataDisableToastDisplayed = false;
     private SubscriptionManager mSubscriptionManager;
+    private boolean mHardcodeDefaultMobileNetworks = false;
 
     public SimSettings() {
         super(DISALLOW_CONFIG_SIM);
@@ -107,6 +108,9 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         if (mSubInfoList == null) {
             mSubInfoList = mSubscriptionManager.getActiveSubscriptionInfoList();
         }
+
+        mHardcodeDefaultMobileNetworks = getResources()
+                .getBoolean(R.bool.config_hardcodeDefaultMobileNetworks);
 
         mNumSlots = tm.getSimCount();
         mPhoneCount = TelephonyManager.getDefault().getPhoneCount();
@@ -190,9 +194,13 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         for (int i = 0; i < mNumSlots; ++i) {
             final SubscriptionInfo sir = findRecordBySlotId(i);
             if (mNumSlots > 1) {
-                mSimEnablers.add(i, new MultiSimEnablerPreference(
-                        getActivity(), sir, mHandler, i));
-                simEnablers.addPreference(mSimEnablers.get(i));
+                MultiSimEnablerPreference multiSimEnablerPreference =
+                        new MultiSimEnablerPreference(getActivity(), sir, mHandler, i);
+                mSimEnablers.add(multiSimEnablerPreference);
+                simEnablers.addPreference(multiSimEnablerPreference);
+                if (mHardcodeDefaultMobileNetworks && i == 0) {
+                    multiSimEnablerPreference.setExplicitlyDisabled(true);
+                }
             } else {
                 removePreference(SIM_ENABLER_CATEGORY);
             }
@@ -456,6 +464,10 @@ public class SimSettings extends RestrictedSettingsFragment implements Indexable
         for (int i = 0; i < subAvailableSize; ++i) {
             final SubscriptionInfo sir = mAvailableSubInfos.get(i);
             if(sir != null){
+                if (i > 0 && (keyPref.equals(KEY_CALLS) || keyPref.equals(KEY_SMS)) &&
+                        mHardcodeDefaultMobileNetworks) {
+                    continue;
+                }
                 simPref.addItem(sir.getDisplayName().toString(), sir);
             }
         }
