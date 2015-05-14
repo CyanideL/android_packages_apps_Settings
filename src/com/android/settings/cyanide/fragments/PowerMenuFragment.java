7abc192d41abc24d8369867d6132d9bbb5ae073c
rogersb11 <brettrogers11@gmail.com>
2015-05-14 16:08:55 -0400

    On the go mode 1/2
        Credits to nameless for the feature and fusion for the forward port

--- src/com/android/settings/cyanide/fragments/PowerMenuFragment.java
+++ src/com/android/settings/cyanide/fragments/PowerMenuFragment.java
@@ -16,12 +16,14 @@
 
 package com.android.settings.cyanide.fragments;
 
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.ListPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -32,11 +34,24 @@ import android.widget.ListView;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class PowerMenuFragment extends SettingsPreferenceFragment {
+public class PowerMenuFragment extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+
+    private SwitchPreference mOnTheGoPowerMenu;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.power_menu_fragment);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mOnTheGoPowerMenu = (SwitchPreference) findPreference(POWER_MENU_ONTHEGO_ENABLED);
+        mOnTheGoPowerMenu.setChecked(Settings.System.getInt(resolver,
+                Settings.System.POWER_MENU_ONTHEGO_ENABLED, 0) == 1);
+        mOnTheGoPowerMenu.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -51,6 +66,16 @@ public class PowerMenuFragment extends SettingsPreferenceFragment {
             list.setPadding(0, paddingTop, 0, paddingBottom);
         }
         return view;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mOnTheGoPowerMenu) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.POWER_MENU_ONTHEGO_ENABLED, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
 }
