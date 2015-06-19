c7f0bcbea1b98a519e2e6f9502ee99afb02e5265
LorDClockaN <davor@losinj.com>
2015-06-19 22:46:59 +0200

    Update HeadsUp tile drawables (2/2)
    Change-Id: I5625dabd098b6bc70240cb83545649c149515397

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -214,7 +214,7 @@ public class QSTileHolder {
                 stringId = R.string.qs_tile_hwkeys;
                 break;
             case QSConstants.TILE_HEADS_UP:
-                resourceName = "ic_qs_heads_up_on";
+                resourceName = "ic_headsup_toggle_on";
                 stringId = R.string.qs_tile_heads_up;
                 break;                
             case QSConstants.TILE_MUSIC:
