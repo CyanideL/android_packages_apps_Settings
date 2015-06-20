AICP/packages_apps_Settings

Settings: Restart SystemUI tile (2/2)
Change-Id: I01557c18f5bbb71fbcbdc2a0a54a5b6ccedb524e

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -205,6 +205,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_heads_up_on";
                 stringId = R.string.qs_tile_heads_up;
                 break;
+            case QSConstants.TILE_SYSTEMUI_REBOOT:
+                resourceName = "ic_qs_reboot";
+                stringId = R.string.qs_systemui_restart_tile;
+                break;
             default:
                 return null;
         }

CyanideL/android_packages_apps_Settings

Conflicts:
	res/values/strings_aicp.xml

src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -233,6 +233,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_pie_global_on";
                 stringId = R.string.qs_pa_pie_control_tile;
                 break;
+            case QSConstants.TILE_SYSTEMUI_RESTART:
+                resourceName = "ic_qs_systemui_restart";
+                stringId = R.string.qs_tile_systemui_restart;
+                break;
             default:
                 return null;
         }