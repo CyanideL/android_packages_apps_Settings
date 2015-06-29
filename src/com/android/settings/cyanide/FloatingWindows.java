5975582a4442491cc52774f3313fd3ae67f83e2e
rogersb11 <brettrogers11@gmail.com>
2015-06-28 20:29:04 -0400

    Heads up floating window switch 2/2

--- src/com/android/settings/cyanide/FloatingWindows.java
+++ src/com/android/settings/cyanide/FloatingWindows.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 CyanideL
+ * Copyright (C) 2015 CyanideL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,24 +16,52 @@
 
 package com.android.settings.cyanide;
 
+
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.os.UserHandle;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 
-public class FloatingWindows extends SettingsPreferenceFragment {
+public class FloatingWindows extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
+
+    private static final String PREF_HEADS_UP_FLOATING =
+	    "heads_up_floating";
+
+    SwitchPreference mHeadsUpFloatingWindow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.floating_windows);
+
+	mHeadsUpFloatingWindow = (SwitchPreference) findPreference(PREF_HEADS_UP_FLOATING);
+	mHeadsUpFloatingWindow.setChecked(Settings.System.getIntForUser(getContentResolver(),
+	Settings.System.HEADS_UP_FLOATING, 1, UserHandle.USER_CURRENT) == 1);
+	mHeadsUpFloatingWindow.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHeadsUpFloatingWindow) {
+	    Settings.System.putIntForUser(getContentResolver(),
+		    Settings.System.HEADS_UP_FLOATING,
+	    (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+	    return true;
+        }
+        return false;
     }
 }
