acb67394d5316117d4ce0e5e816ca8d1a76c1ae1
rogersb11 <brettrogers11@gmail.com>
2015-07-06 21:56:09 -0400

    Floating windows changeup
    Add reset options and move them all to java to do so
    Left the none working switches for now since not releasing yet and easy to just hide them anyway

--- src/com/android/settings/cyanide/FloatingWindows.java
+++ src/com/android/settings/cyanide/FloatingWindows.java
@@ -16,18 +16,24 @@
 
 package com.android.settings.cyanide;
 
-
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
-import android.os.UserHandle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -37,45 +43,184 @@ import java.util.List;
 public class FloatingWindows extends SettingsPreferenceFragment
             implements OnPreferenceChangeListener  {
 
-    private static final String PREF_HEADS_UP_FLOATING =
-	    "heads_up_floating";
-	private static final String GESTURE_ANYWHERE_FLOATING =
-	    "gesture_anywhere_floating";
+    private static final String FLOATING_WINDOW_MODE = "floating_window_mode";
+    private static final String GESTURE_ANYWHERE_FLOATING = "gesture_anywhere_floating";
+    private static final String HEADS_UP_FLOATING = "heads_up_floating";
+    private static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+    private static final String TASK_MANAGER_FLOATING = "task_manager_floating";
 
-    SwitchPreference mHeadsUpFloatingWindow;
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
+
+    SwitchPreference mFloatingWindowMode;
     SwitchPreference mGestureAnywhereFloatingWindow;
+    SwitchPreference mHeadsUpFloatingWindow;
+    SwitchPreference mSlimActionFloatingWindow;
+    SwitchPreference mTaskManagerFloatingWindow;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.floating_windows);
+        mResolver = getActivity().getContentResolver();
 
-    mHeadsUpFloatingWindow = (SwitchPreference) findPreference(PREF_HEADS_UP_FLOATING);
-    mHeadsUpFloatingWindow.setChecked(Settings.System.getIntForUser(getContentResolver(),
-            Settings.System.HEADS_UP_FLOATING, 0, UserHandle.USER_CURRENT) == 1);
-    mHeadsUpFloatingWindow.setOnPreferenceChangeListener(this);
+        mFloatingWindowMode = (SwitchPreference) findPreference(FLOATING_WINDOW_MODE);
+        mFloatingWindowMode.setChecked(Settings.System.getInt(mResolver,
+            Settings.System.FLOATING_WINDOW_MODE, 0) == 1);
+        mFloatingWindowMode.setOnPreferenceChangeListener(this);
 
-    mGestureAnywhereFloatingWindow = (SwitchPreference) findPreference(GESTURE_ANYWHERE_FLOATING);
-    mGestureAnywhereFloatingWindow.setChecked(Settings.System.getIntForUser(getContentResolver(),
-            Settings.System.GESTURE_ANYWHERE_FLOATING, 0, UserHandle.USER_CURRENT) == 1);
-    mGestureAnywhereFloatingWindow.setOnPreferenceChangeListener(this);
+        mGestureAnywhereFloatingWindow = (SwitchPreference) findPreference(GESTURE_ANYWHERE_FLOATING);
+        mGestureAnywhereFloatingWindow.setChecked(Settings.System.getInt(mResolver,
+            Settings.System.GESTURE_ANYWHERE_FLOATING, 0) == 1);
+        mGestureAnywhereFloatingWindow.setOnPreferenceChangeListener(this);
 
+        mHeadsUpFloatingWindow = (SwitchPreference) findPreference(HEADS_UP_FLOATING);
+        mHeadsUpFloatingWindow.setChecked(Settings.System.getInt(mResolver,
+            Settings.System.HEADS_UP_FLOATING, 0) == 1);
+        mHeadsUpFloatingWindow.setOnPreferenceChangeListener(this);
+
+        mSlimActionFloatingWindow = (SwitchPreference) findPreference(SLIM_ACTION_FLOATS);
+        mSlimActionFloatingWindow.setChecked(Settings.System.getInt(mResolver,
+            Settings.System.SLIM_ACTION_FLOATS, 0) == 1);
+        mSlimActionFloatingWindow.setOnPreferenceChangeListener(this);
+
+        mTaskManagerFloatingWindow = (SwitchPreference) findPreference(TASK_MANAGER_FLOATING);
+        mTaskManagerFloatingWindow.setChecked(Settings.System.getInt(mResolver,
+            Settings.System.TASK_MANAGER_FLOATING, 0) == 1);
+        mTaskManagerFloatingWindow.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup_restore)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mHeadsUpFloatingWindow) {
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.HEADS_UP_FLOATING,
-            (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+        if (preference == mFloatingWindowMode) {
+            Settings.System.putInt(mResolver,
+                    Settings.System.FLOATING_WINDOW_MODE,
+            (Boolean) newValue ? 1 : 0);
             return true;
-        } else if (preference == mGestureAnywhereFloatingWindow) {
-            Settings.System.putIntForUser(getContentResolver(),
+       } else if (preference == mGestureAnywhereFloatingWindow) {
+            Settings.System.putInt(mResolver,
                     Settings.System.GESTURE_ANYWHERE_FLOATING,
-            (Boolean) newValue ? 1 : 0, UserHandle.USER_CURRENT);
+            (Boolean) newValue ? 1 : 0);
+            return true;
+       } else if (preference == mHeadsUpFloatingWindow) {
+            Settings.System.putInt(mResolver,
+                    Settings.System.HEADS_UP_FLOATING,
+            (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mSlimActionFloatingWindow) {
+            Settings.System.putInt(mResolver,
+                    Settings.System.SLIM_ACTION_FLOATS,
+            (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mTaskManagerFloatingWindow) {
+            Settings.System.putInt(mResolver,
+                    Settings.System.TASK_MANAGER_FLOATING,
+            (Boolean) newValue ? 1 : 0);
             return true;
         }
         return false;
+    }
+    
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        FloatingWindows getOwner() {
+            return (FloatingWindows) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.reset_android,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.FLOATING_WINDOW_MODE, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.GESTURE_ANYWHERE_FLOATING, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.HEADS_UP_FLOATING, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.SLIM_ACTION_FLOATS, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.TASK_MANAGER_FLOATING, 0);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.reset_cyanide,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.FLOATING_WINDOW_MODE, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.GESTURE_ANYWHERE_FLOATING, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.HEADS_UP_FLOATING, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.SLIM_ACTION_FLOATS, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.TASK_MANAGER_FLOATING, 1);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 }
