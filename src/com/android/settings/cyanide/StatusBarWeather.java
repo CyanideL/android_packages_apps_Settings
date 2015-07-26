5a8a87086089fab4a524d2ab4c61d3d0615375cf
rogersb11 <brettrogers11@gmail.com>
2015-07-26 18:05:06 -0400

    Add reset options to weather

--- src/com/android/settings/cyanide/StatusBarWeather.java
+++ src/com/android/settings/cyanide/StatusBarWeather.java
@@ -15,8 +15,12 @@
  */
 package com.android.settings.cyanide;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -24,7 +28,11 @@ import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.android.settings.R;
@@ -44,19 +52,34 @@ public class StatusBarWeather extends SettingsPreferenceFragment
     private static final String STATUS_BAR_TEMPERATURE_STYLE = "status_bar_temperature_style";
     private static final String PREF_STATUS_BAR_WEATHER_COLOR = "status_bar_weather_color";
 
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
+
+    private static final int CYANIDE_BLUE = 0xff1976D2;
+
     private ListPreference mStatusBarTemperature;
     private ListPreference mStatusBarTemperatureStyle;
     private ColorPickerPreference mStatusBarTemperatureColor;
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.status_bar_weather);
+    private ContentResolver mResolver;
 
-        ContentResolver resolver = getActivity().getContentResolver();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        addPreferencesFromResource(R.xml.status_bar_weather);
+        mResolver = getActivity().getContentResolver();
 
         mStatusBarTemperature = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE);
-        int temperatureShow = Settings.System.getIntForUser(resolver,
+        int temperatureShow = Settings.System.getIntForUser(mResolver,
                 Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
                 UserHandle.USER_CURRENT);
         mStatusBarTemperature.setValue(String.valueOf(temperatureShow));
@@ -64,7 +87,7 @@ public class StatusBarWeather extends SettingsPreferenceFragment
         mStatusBarTemperature.setOnPreferenceChangeListener(this);
 
         mStatusBarTemperatureStyle = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE_STYLE);
-        int temperatureStyle = Settings.System.getIntForUser(resolver,
+        int temperatureStyle = Settings.System.getIntForUser(mResolver,
                 Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0,
                 UserHandle.USER_CURRENT);
         mStatusBarTemperatureStyle.setValue(String.valueOf(temperatureStyle));
@@ -74,22 +97,41 @@ public class StatusBarWeather extends SettingsPreferenceFragment
         mStatusBarTemperatureColor =
             (ColorPickerPreference) findPreference(PREF_STATUS_BAR_WEATHER_COLOR);
         mStatusBarTemperatureColor.setOnPreferenceChangeListener(this);
-        int intColor = Settings.System.getInt(resolver,
+        int intColor = Settings.System.getInt(mResolver,
                 Settings.System.STATUS_BAR_WEATHER_COLOR, 0xffffffff);
         String hexColor = String.format("#%08x", (0xffffffff & intColor));
         mStatusBarTemperatureColor.setSummary(hexColor);
         mStatusBarTemperatureColor.setNewPreviewColor(intColor);
 
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_backup_restore)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mStatusBarTemperature) {
             int temperatureShow = Integer.valueOf((String) newValue);
             int index = mStatusBarTemperature.findIndexOfValue((String) newValue);
             Settings.System.putIntForUser(
-                    resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureShow,
+                    mResolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureShow,
                     UserHandle.USER_CURRENT);
             mStatusBarTemperature.setSummary(
                     mStatusBarTemperature.getEntries()[index]);
@@ -98,7 +140,7 @@ public class StatusBarWeather extends SettingsPreferenceFragment
             int temperatureStyle = Integer.valueOf((String) newValue);
             int index = mStatusBarTemperatureStyle.findIndexOfValue((String) newValue);
             Settings.System.putIntForUser(
-                    resolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, temperatureStyle,
+                    mResolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, temperatureStyle,
                     UserHandle.USER_CURRENT);
             mStatusBarTemperatureStyle.setSummary(
                     mStatusBarTemperatureStyle.getEntries()[index]);
@@ -108,11 +150,77 @@ public class StatusBarWeather extends SettingsPreferenceFragment
                     Integer.valueOf(String.valueOf(newValue)));
             preference.setSummary(hex);
             int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(mResolver,
                     Settings.System.STATUS_BAR_WEATHER_COLOR, intHex);
             return true;
         }
         return false;
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        StatusBarWeather getOwner() {
+            return (StatusBarWeather) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.reset_android,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_WEATHER_COLOR,
+                                    0xffffffff);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.reset_cyanide,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_WEATHER_COLOR,
+                                    CYANIDE_BLUE);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 
 }
