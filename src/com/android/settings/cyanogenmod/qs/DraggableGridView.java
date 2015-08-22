89ee75ff73c23faf76a568fd913520f27a85f623
Roman Birg <roman@cyngn.com>
2015-08-22 00:02:35 -0400

    Settings: add a way to reset quick settings tiles
    - Also convert the add tile dialog into a dialog fragment so it persists
      throughout rotations
    - remove click sounds from toggle arrange view

--- src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
+++ src/com/android/settings/cyanogenmod/qs/DraggableGridView.java
@@ -84,6 +84,7 @@ public class DraggableGridView extends ViewGroup implements
 
         setListeners();
         setChildrenDrawingOrderEnabled(true);
+        setSoundEffectsEnabled(false);
     }
 
     public void setUseLargeFirstRow(boolean largeFirstRow) {
@@ -132,6 +133,13 @@ public class DraggableGridView extends ViewGroup implements
         super.removeViewAt(index);
         mNewPositions.remove(index);
     };
+
+    public void resetState() {
+        removeAllViews();
+        mNewPositions.clear();
+        mLastTarget = -1;
+        mDragged = -1;
+    }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
