afb2215924f72ac09e22329e07695724237bc3d3
rogersb11 <brettrogers11@gmail.com>
2015-09-04 07:28:20 -0400

    Settings: Make all 14 font variants available to Statusbar Clock and Weather (2/2)
    Credit to temasek for clock and the idea obviously

--- src/com/android/settings/cyanide/StatusBarClockStyle.java
+++ src/com/android/settings/cyanide/StatusBarClockStyle.java
@@ -132,7 +132,7 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
         mFontStyle.setOnPreferenceChangeListener(this);
         mFontStyle.setValue(Integer.toString(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.STATUSBAR_CLOCK_FONT_STYLE,
-                4)));
+                0)));
         mFontStyle.setSummary(mFontStyle.getEntry());
 
         mClockAmPmStyle = (ListPreference) prefSet.findPreference(PREF_AM_PM_STYLE);
