AOKP/packages_apps_Settings

RunningServices activity now points to Running services, not Manage apps
Change-Id: Ic33311d3886dc0a0c45dc6143efac63d2e3c3ff3

src/com/android/settings/SettingsActivity.java
@@ -87,6 +87,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
@@ -397,7 +398,8 @@ public class SettingsActivity extends Activity
             SmartbarSettings.class.getName(),
             LiveLockScreenSettings.class.getName(),
             HeadsUpSettings.class.getName(),
-            DozeSettingsFragment.class.getName()
+            DozeSettingsFragment.class.getName(),
+            RunningServices.class.getName()
     };
 
 
@@ -946,7 +948,6 @@ public class SettingsActivity extends Activity
         if (intentClass.equals(getClass().getName())) return null;
 
         if ("com.android.settings.ManageApplications".equals(intentClass)
-                || "com.android.settings.RunningServices".equals(intentClass)
                 || "com.android.settings.applications.StorageUse".equals(intentClass)) {
             // Old names of manage apps.
             intentClass = com.android.settings.applications.ManageApplications.class.getName();

CyanideL/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
@@ -409,7 +410,8 @@ public class SettingsActivity extends Activity
             VolumeDialogSettings.class.getName(),
             ExpansionViewBatterySettings.class.getName(),
             ExpansionViewTraffic.class.getName(),
-            StatusBarExpandedHeaderSettings.class.getName()
+            StatusBarExpandedHeaderSettings.class.getName(),
+            RunningServices.class.getName()
     };
 
 
@@ -953,7 +955,6 @@ public class SettingsActivity extends Activity
         if (intentClass.equals(getClass().getName())) return null;
 
         if ("com.android.settings.ManageApplications".equals(intentClass)
-                || "com.android.settings.RunningServices".equals(intentClass)
                 || "com.android.settings.applications.StorageUse".equals(intentClass)) {
             // Old names of manage apps.
             intentClass = com.android.settings.applications.ManageApplications.class.getName();